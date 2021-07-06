69414b3453713f5eae5ae1b99349405bd1635b48
n-x-1000 <you@example.com>
2021-07-06 19:37:14 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Login from "./Components/Login";
 import SignUp from "./Components/SignUp";
 
 function App() {
   return (
     <>
-      <SignUp />
+      {/* <SignUp /> */}
+      <Login />
     </>
   );
 }
---
+++ src/Components/Logintosignup/Logintosignup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Logintosignup.css";
+function Logintosignup(){
+    return(
+        <div id="tosignup">
+            <div id="signup">
+                <p>Create an account? Signup </p>
+            </div>
+        </div>
+    );
+}
+export default Logintosignup;
