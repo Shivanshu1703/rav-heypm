dad9873491d34bdf0ece49b4d5419769cfe17ac2
NITISH KUMAR <imnitishkumar6969@gmail.com>
2021-07-12 20:40:28 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from "./Components/Login";
 import SignUp from "./Components/SignUp";
 import { Route, Switch } from "react-router-dom";
 import Profilepage from "./Components/profile/profilepage";
+import Landingpage from "./Components/home/landingpage";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/profilepage" component={Profilepage}/>
+        <Route exact path="/landingpage" component={Landingpage}/>
       </Switch>
     </>
   );
